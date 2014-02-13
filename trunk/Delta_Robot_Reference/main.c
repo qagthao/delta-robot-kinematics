d7d6888c8c1844ca1084e8cc7cf8cb9e4d520ef3
rampadc@gmail.com <rampadc@gmail.com@d879f86e-284c-b558-9542-1f396b516044>
2014-02-13 05:33:38 +0000

    User interface prepared, now working on ways to handle commands

--- trunk/Delta_Robot_Reference/main.c
+++ trunk/Delta_Robot_Reference/main.c
@@ -2,11 +2,13 @@
 
 //application related
 int choice;
+char command[256];
 
 void createMenu();
 void printModelParameters();
 void setModelParameters();
 void listCommands();
+void process(char *);
 
 //Delta robot parameters
 float e = 115.0;     // end effector's side length
@@ -17,11 +19,23 @@ float rf = 112.0;	 // upper arm
 int main(int argc, char **argv)
 {
 	createMenu();
-	
+
+	while(1) {
+		fseek(stdin,0,SEEK_END);
+		printf(">> ");
+		if(fgets(command, 256, stdin) != NULL) {
+			process(command);
+		}
+	}
 	return 0;
 }
 
-void createMenu() {
+void process(char * instr) {
+	printf("%s", instr);
+}
+
+void createMenu()
+{
 	printf("Delta Robot Kinematics Reference\n\n");
 	printf("Choose the following settings:\n");
 	printf("\t1. View current delta robot model parameters\n");
@@ -30,30 +44,31 @@ void createMenu() {
 	printf("\t4. Start\n\n");
 	printf("Choice: ");
 	scanf("%d", &choice);
-	
+
 	while(choice > 0 && choice < 5) {
 		switch(choice) {
-			case 1:
-				printf("\n\n");
-				printModelParameters();
-				break;
-			case 2:
-				printf("\n\n");
-				setModelParameters();
-				break;
-			case 3:
-				printf("\n\n");
-				listCommands();
-				break;
-			case 4:
-				printf("\n\n");
-				return;
-				break;
+		case 1:
+			printf("\n\n");
+			printModelParameters();
+			break;
+		case 2:
+			printf("\n\n");
+			setModelParameters();
+			break;
+		case 3:
+			printf("\n\n");
+			listCommands();
+			break;
+		case 4:
+			printf("\n\n");
+			return;
+			break;
 		}
 	}
 }
 
-void printModelParameters() {
+void printModelParameters()
+{
 	printf("Current model parameters:\n");
 	printf("%-20s%-20s%-20s%-20s\n", "Base", "End effector", "Lower arm", "Upper arm");
 	printf("%-20f%-20f%-20f%-20f\n", f, e, re, rf);
@@ -61,16 +76,31 @@ void printModelParameters() {
 	createMenu();
 }
 
-void setModelParameters() {
-	printf("New model parameters (use arrows to get to next/previous fields)\n");
-	printf("%-20s%-20s%-20s%-20s\n", "Base", "End effector", "Lower arm", "Upper arm");
-	
+void setModelParameters()
+{
+	printf("New model parameters:\n");
+	printf("Base length: ");
+	scanf("%f", &f);
+	printf("End effector length: ");
+	scanf("%f", &e);
+	printf("Lower arm length: ");
+	scanf("%f", &re);
+	printf("Upper arm length: ");
+	scanf("%f", &rf);
+
 	printf("\n\n");
 	createMenu();
 }
 
-void listCommands() {
-	
+void listCommands()
+{
+	printf("Commands list:\n");
+	printf("%-15s%-60s\n"," -i x, y, z","calculate upper arm angles from end effector's coordinates");
+	printf("%-15s%-60s\n"," -f a1, a2, a3","calculate end effector's coordinates from upper arm angles");
+	printf("%-15s%-60s\n"," -m","show menu");
+	printf("%-15s%-60s\n"," -q","quit without saving model parameters");
+	printf("%-15s%-60s\n"," -sq","save model parameters then quit");
+
 	printf("\n\n");
 	createMenu();
 }
